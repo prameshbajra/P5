d1aaaf47d074ba00876b3d3e23877ec54f31aabe
Suzal3579 <pe.messh@gmail.com>
2017-02-19 22:04:54 +0530

    Soundworks fixed ... working now ... button bugs present.

--- soundwork.js
+++ soundwork.js
@@ -1,14 +1,45 @@
 var song;
+var sliderVolume;
+var sliderRate;
+var sliderPan;
+var buttonP;
+var buttonS;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    song = loadSound("mario.mp3", loaded);
+    song = loadSound("mario.mp3");
+    sliderVolume = createSlider(0, 1, 0.5, 0.1);
+    sliderRate = createSlider(0, 2, 1, 0.1);
+    sliderPan = createSlider(-1, 1, 0, 0.5);
+    buttonS = createButton();
+    buttonS.mousePressed(toggleButton2);
+    buttonP = createButton();
+    buttonP.mousePressed(toggleButton);
 }
 
-function loaded() {
-    song.play();
+function toggleButton2() {
+    if (song.isPlaying()) {
+        song.stop();
+        buttonS.html("Play From The Start");
+    } else {
+        song.play();
+        buttonS.html("Stop now");
+    }
+}
+
+function toggleButton() {
+    if (song.isPlaying) {
+        buttonP.html("Pause");
+        song.pause();
+    } else {
+        buttonP.html("Play");
+        song.play();
+    }
 }
 
 function draw() {
-    background(random(255), random(255), random(255));
+    background(song.currentTime() * 20, 100, 0);
+    song.setVolume(sliderVolume.value());
+    song.rate(sliderRate.value());
+    song.pan(sliderPan.value());
 }
