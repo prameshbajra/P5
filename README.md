283dc89869ab225524b4d534bf64ed52e8e115bf
Suzal3579 <pe.messh@gmail.com>
2017-02-26 20:40:21 +0530

    TextBursting continues

--- README.md
+++ README.md
@@ -12,3 +12,5 @@ Collection of all P5.js works.
          >> Download/Clone the whole repo and run HTML files. Works sWell!
     
 	     >> HTML and JAVASCRIPT files have common names to avoid confusion.
+
+         >> To run "TEXTBURST" your need localhost server. (For eg: XAMPP(APACHE SERVER) in windows would do!)
--- TextBurst/textburst.js
+++ TextBurst/textburst.js
@@ -1,8 +1,15 @@
+var f;
+
+function preload() {
+    f = loadFont("britanic.ttf");
+}
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    background(51);
-    textFont("Georgia");
-    var points = font.textToPoints("Pramesh", 100, 200, 200);
+    background(0);
+    textFont(f);
+    text("Suzal", 100, 200);
+    var points = f.textToPoints("Pramesh", 100, 200, 200);
     for (var i = 0; i < points.length; i++) {
         var pt = points[i];
         stroke(255);
