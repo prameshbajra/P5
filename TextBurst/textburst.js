37d3d8d007413f25e70a8e63939ce2a506168c94
Suzal3579 <pe.messh@gmail.com>
2017-02-26 21:17:07 +0530

    Dissociation working ...

--- TextBurst/textburst.js
+++ TextBurst/textburst.js
@@ -1,23 +1,26 @@
 var f;
+var particle = [];
 
 function preload() {
-    f = loadFont("britanic.ttf");
+    f = loadFont("bauhs93.ttf");
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    background(0);
     textFont(f);
-    text("Suzal", 100, 200);
-    var points = f.textToPoints("Pramesh", 100, 200, 200);
+    var points = f.textToPoints("Pramesh", 350, 430, 300);
     for (var i = 0; i < points.length; i++) {
         var pt = points[i];
-        stroke(255);
-        strokeWeight(4);
-        point(pt.x, pt.y);
+        var particles = new Particles(pt.x, pt.y);
+        particle.push(particles);
     }
 }
 
 function draw() {
-
+    background(0);
+    for (var i = 0; i < particle.length; i++) {
+        var value = particle[i];
+        value.update();
+        value.show();
+    }
 }
