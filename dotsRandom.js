b56754f2f2f1da61379e4ad11a8b5de98689c27d
Suzal3579 <pe.messh@gmail.com>
2017-03-08 12:51:12 +0530

    To be remade later ...

--- dotsRandom.js
+++ dotsRandom.js
@@ -9,10 +9,7 @@ function setup() {
 function draw() {
     fill(random(1, 255), random(1, 255), random(1, 255));
     noStroke();
-    count++;
     ellipse(random(0, windowWidth), random(0, windowHeight), 10, 10);
-    if (count > 200) {
-        fill(255);
-        ellipse(random(0, windowWidth), random(0, windowHeight), 50, 50);
-    }
+    fill(255);
+    ellipse(random(0, windowWidth), random(0, windowHeight), 10, 10);
 }
