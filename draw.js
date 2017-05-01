abd26e95e2d9b2c0ae8d9ca5650511c9bd80bd85
Suzal3579 <pe.messh@gmail.com>
2017-05-01 21:55:45 +0545

    making a ludo cube

--- draw.js
+++ draw.js
@@ -1,10 +1,17 @@
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-    background(255, 150, 150);
+var sizeOfCube = 400;
 
+function setup() {
+    createCanvas(windowWidth, windowHeight, WEBGL);
 }
 
 function draw() {
-    fill(random(0, 255), random(0, 255), random(0, 255));
-    ellipse(random(0, windowWidth), random(0, windowHeight), 20, 20);
+    background(250);
+    translate(0, 0, sizeOfCube);
+    if (sizeOfCube > 100) {
+        sizeOfCube -= 10;
+    }
+    rotateX(frameCount * 0.1);
+    rotateY(frameCount * 0.1);
+    rotateZ(frameCount * 0.1);
+    box(70, 70, 70);
 }
