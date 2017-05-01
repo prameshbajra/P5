bd1d2fdcc4e4fb5429076d485d9a12981caef4ba
Suzal3579 <pe.messh@gmail.com>
2017-05-01 23:03:57 +0545

    more thing to be done

--- draw.js
+++ draw.js
@@ -1,4 +1,6 @@
-var sizeOfCube = 400;
+let sizeOfCube = 400,
+    timer = 1000;
+
 
 function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
@@ -10,8 +12,12 @@ function draw() {
     if (sizeOfCube > 100) {
         sizeOfCube -= 10;
     }
-    rotateX(frameCount * 0.1);
-    rotateY(frameCount * 0.1);
-    rotateZ(frameCount * 0.1);
+    rotateX(frameCount * 0.01);
+    rotateY(frameCount * 0.01);
+    rotateZ(frameCount * 0.01);
     box(70, 70, 70);
+    timer -= 10;
+    if (timer == 0) {
+        noLoop();
+    }
 }
