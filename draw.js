52e1fcc79d996d25bff76ef9c76569f4e441d47f
Suzal3579 <pe.messh@gmail.com>
2017-05-02 13:48:49 +0545

    Two cube made .. None to be processed further ...

--- draw.js
+++ draw.js
@@ -1,28 +1,73 @@
-let sizeOfCube = 400,
-    timer = 1000,
-    img;
-
-function preload() {
-    img = loadImage("olo/fgfg.jpg");
-}
+let toSize = 200;
 
 function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
+    colorMode(RGB, 1);
 }
 
 function draw() {
-    background(250);
-    translate(0, 0, sizeOfCube);
-    if (sizeOfCube > 100) {
-        sizeOfCube -= 10;
-    }
-    rotateX(frameCount * 0.1);
-    rotateY(frameCount * (0.1));
-    rotateZ(frameCount * 0.1);
-    texture(img);
-    box(70, 70, 70);
-    timer -= 10;
-    if (timer == 0) {
-        noLoop();
+    translate(20, 20, toSize);
+    background(255);
+    strokeWeight(4);
+    scale(90);
+    rotateX(mouseX * 0.01);
+    rotateY(mouseY * 0.01);
+    strokeWeight(30);
+    // Back side view ...
+    beginShape();
+    fill(1, 0, 0);
+    vertex(0, 0, 0);
+    vertex(0, 1, 0);
+    vertex(1, 1, 0);
+    vertex(1, 0, 0);
+    vertex(0, 0, 0);
+    endShape(CLOSE);
+
+    // left side view ...
+    beginShape();
+    fill(0, 1, 0);
+    vertex(0, 0, 0);
+    vertex(0, 0, 1);
+    vertex(0, 1, 1);
+    vertex(0, 1, 0);
+    endShape(CLOSE);
+
+    // Top view ...
+    beginShape();
+    fill(0, 0, 1);
+    vertex(1, 0, 0);
+    vertex(1, 0, 1);
+    vertex(0, 0, 1);
+    vertex(0, 0, 0);
+    endShape(CLOSE);
+
+    // Front view ...
+    beginShape();
+    fill(1, 1, 0);
+    vertex(1, 0, 1);
+    vertex(1, 1, 1);
+    vertex(0, 1, 1);
+    vertex(0, 0, 1);
+    endShape(CLOSE);
+
+    // Right view ...
+    beginShape();
+    fill(0, 1, 1);
+    vertex(1, 0, 1);
+    vertex(1, 0, 0);
+    vertex(1, 1, 0);
+    vertex(1, 1, 1);
+    endShape(CLOSE);
+
+    beginShape();
+    fill(0);
+    vertex(1, 1, 1);
+    vertex(0, 1, 1);
+    vertex(0, 1, 0);
+    vertex(1, 1, 0);
+    endShape(CLOSE);
+
+    if (toSize > 50) {
+        toSize -= 10;
     }
 }
